cf3b20cac080a83ff72d9612e0c4ae2086f986f0
FilipKufalov <filip.kufalov@hotmail.com>
2023-12-28 18:58:15 +0100

    fixes

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 // Reference from https://stackoverflow.com/questions/72090694/best-way-to-read-an-xlsx-file-in-vanilla-javascript
 // npm i xlsx
 
-(async() => {
-    const url = "Book1.xlsx"; // your online or local xlsx
-    const data = await (await fetch(url)).arrayBuffer();
-    const workbook = XLSX.read(data);
-    manageData(workbook.Strings);
-  })();
+(async () => {
+  const url = "Book1.xlsx"; // your online or local xlsx
+  const data = await (await fetch(url)).arrayBuffer();
+  const workbook = XLSX.read(data);
+  manageData(workbook);
+})();
 
-  var div = document.getElementById("employeeTable")
+var div = document.getElementById("employeeTable")
 
-  function manageData(data) {
+function manageData(data) {
+  let table = data.Sheets.Sheet1;
+  var values = Object.keys(table).map(key => table[key].v);
+  values = values.filter(function (element) {
+    return element !== undefined;
+  });
 
-    var table = document.createElement("table");
-    var counter = 3                         // 3 depends on rows in xlsx file
-    for (let i = 0; i < data.Count; i++) {  // ↓↓↓↓ 
-        if (counter % 3 == 0)               // same here (my file contains 3 properties (FirstName,LastName & Email))
-        {
-            var tr = document.createElement("tr")
-        }
-        var tableData = document.createElement("td")
-        tableData.innerText = data[i].h
-        tr.append(tableData)
-        table.append(tr)
-        div.append(table)
-        counter++;
+  let table2 = document.createElement("table")
+  var counter = 3
+  for (let i = 0; i < values.length; i++) {
+    if (counter % 3 == 0) {
+      var tr = document.createElement("tr")
     }
+    var tableData = document.createElement("td")
+    tableData.innerText = values[i]
+    tr.append(tableData)
+    table2.append(tr)
+    div.append(table2)
+    counter++;
   }
+}
